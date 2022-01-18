034e8abda69476f58ea77628acb8fee58b6f9379
George Gebbett <george.gebbett1@gmail.com>
2022-01-18 11:21:39 +0000

    Create switch between consume/purchase mode

--- key_grab.py
+++ key_grab.py
@@ -17,6 +17,6 @@ def grab_key_in_thread(app_instance):
     keypad = evdev.InputDevice(device_path)
     keypad.grab()
     for event in keypad.read_loop():
-        print(f"Type: {event.type!s} Code: {event.code!s} Value: {event.value!s}")
+        # print(f"Type: {event.type!s} Code: {event.code!s} Value: {event.value!s}")
         if event.type == evdev.ecodes.EV_KEY and event.value == 1:
             app_instance.active_frame.interpret_keypress(event.code)
