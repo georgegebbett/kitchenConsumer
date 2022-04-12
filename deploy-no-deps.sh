75eee177f63940be3d98ed1198894860e007a7df
George Gebbett <george.gebbett1@gmail.com>
2022-04-12 12:39:05 +0100

    update my IP addresses in deploy scripts

--- deploy-no-deps.sh
+++ deploy-no-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-scp ~/PycharmProjects/kitchenConsumer/* pi@192.168.4.131:/home/pi/barcode/kitchenConsumer
+scp ~/PycharmProjects/kitchenConsumer/* pi@192.168.1.132:/home/pi/barcode/kitchenConsumer
 ssh pi@kitchen.local 'sudo systemctl restart kitchen-consumer.service'
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-scp -r ~/PycharmProjects/kitchenConsumer pi@192.168.4.131:/home/pi/barcode
+scp -r ~/PycharmProjects/kitchenConsumer pi@192.168.1.132:/home/pi/barcode
 ssh pi@kitchen.local 'sudo systemctl restart kitchen-consumer.service'
