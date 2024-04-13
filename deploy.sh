83f9fcf208ba1fb0ceeefe8d548707fa1cf85650
George Gebbett <george.gebbett1@gmail.com>
2024-04-13 15:22:59 +0100

    fix the git setup

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-scp -r ~/PycharmProjects/kitchenConsumer pi@192.168.1.132:/home/pi/barcode
-ssh pi@kitchen.local 'sudo systemctl restart kitchen-consumer.service'
+scp -r ~/PycharmProjects/kitchenConsumer pi@192.168.0.223:/home/pi/barcode
+#ssh pi@kitchen.local 'sudo systemctl restart kitchen-consumer.service'
